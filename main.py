792ac2b183499cc2a1a434c8a9d7506448ef5270
SASITH IMARSHA <sasithimarsha776@gmail.com>
2026-08-16 16:47:46 +0530

    Optimized webhook connections to reduce flood control

--- main.py
+++ main.py
@@ -82,8 +82,9 @@ if __name__ == "__main__":
     print("Bot එක Webhook සමඟ පණ ගැහෙමින් පවතී...")
     
     # Run Webhook
-    application.run_webhook(
+       application.run_webhook(
         listen="0.0.0.0",
         port=int(os.environ.get("PORT", 10000)),
-        url_path="/webhook"
+        url_path="/webhook",
+        max_connections=5  # 🌟 මේ පේළිය එකතු කරන්න!
     )
